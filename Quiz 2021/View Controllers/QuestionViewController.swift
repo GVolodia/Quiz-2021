541c3c3bc46d2c97f051e1639c60f1ff09eb158a
Aficionado <volodiag@mail.ru>
2021-12-09 09:47:27 +0200

    Finished the project

--- Quiz 2021/View Controllers/QuestionViewController.swift
+++ Quiz 2021/View Controllers/QuestionViewController.swift
@@ -8,22 +8,133 @@
 import UIKit
 
 class QuestionViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var singleStackView: UIStackView!
+    @IBOutlet var singleButtons: [UIButton]!
+    
+    @IBOutlet weak var multipleStackView: UIStackView!
+    @IBOutlet var multiLabels: [UILabel]!
+    @IBOutlet var multiSwitches: [UISwitch]!
+    
+    @IBOutlet weak var rangedStackView: UIStackView!
+    @IBOutlet weak var rangedSlider: UISlider!
+    @IBOutlet var rangedLabels: [UILabel]!
+    
+    @IBOutlet weak var questionProgressView: UIProgressView!
+    
+    
+    var questionIndex = 0
+    var answerChosen = [Answer]() {
+        didSet {
+            print(#line, #function, answerChosen)
+        }
+    }
+    var currentAnswers: [Answer] {
+        currentQuestion.answers
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    var currentQuestion: Question {
+        Question.all[questionIndex]
     }
-    */
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+    
+    func updateUI() {
+        singleStackView.isHidden = true
+        multipleStackView.isHidden = true
+        rangedStackView.isHidden = true
+        
+        func updateSingleStack() {
+            singleStackView.isHidden = false
+            for (index, button) in singleButtons.enumerated() {
+                button.setTitle(nil, for: [])
+                button.tag = index
+            }
+            for (button, answer) in zip(singleButtons, currentAnswers) {
+                button.setTitle(answer.text, for: [])
+            }
+        }
+        
+        func updateMultipleStack() {
+            multipleStackView.isHidden = false
+            for label in multiLabels {
+                label.text = nil
+            }
+            for (label, answer) in zip(multiLabels, currentAnswers) {
+                label.text = answer.text
+            }
+        }
+        
+        func updateRangedStack() {
+            rangedStackView.isHidden = false
+            rangedLabels.first?.text = currentAnswers.first?.text
+            rangedLabels.last?.text = currentAnswers.last?.text
+        }
+        
+        let totalProgress = Float(questionIndex) / Float(Question.all.count)
+        questionLabel.text = currentQuestion.text
+        questionProgressView.setProgress(totalProgress, animated: true)
+        
+        navigationItem.title = "Вопрос № \(questionIndex + 1)"
+        
+        switch currentQuestion.type {
+        case .single:
+            updateSingleStack()
+        case .multiple:
+            updateMultipleStack()
+        case .range:
+            updateRangedStack()
+        }
+        
+    }
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        let VC = segue.destination as! ResultsViewController
+//        VC.answers = answerChosen
+//    }
+    
+    func nextQuestion() {
+        questionIndex += 1
+        if questionIndex < Question.all.count {
+            updateUI()
+        } else {
+            performSegue(withIdentifier: "Results Segue", sender: nil)
+        }
+    }
+    
+    @IBAction func singleButtonPressed(_ sender: UIButton) {
+        let index = sender.tag
+        guard 0 <= index && index < currentAnswers.count else {
+            return
+        }
+        let answer = currentAnswers[index]
+        answerChosen.append(answer)
+        nextQuestion()
+    }
+    
+    @IBAction func multiButtonPressed() {
+        for (index, multiSwitch) in multiSwitches.enumerated() {
+            if multiSwitch.isOn && index < currentAnswers.count {
+                let answer = currentAnswers[index]
+                answerChosen.append(answer)
+            }
+        }
+        nextQuestion()
+    }
+    
+    @IBAction func rangedButtonPressed() {
+        let index = Int(round(rangedSlider.value*(Float(currentAnswers.count)-1)))
+        if index < currentAnswers.count {
+            let answer = currentAnswers[index]
+            answerChosen.append(answer)
+        }
+        nextQuestion()
+    }
+    @IBSegueAction func resultsSegue(_ coder: NSCoder) -> ResultsViewController? {
+        return ResultsViewController(coder: coder, answerChosen)
+    }
 }
--- Quiz 2021/View Controllers/ResultsViewController.swift
+++ Quiz 2021/View Controllers/ResultsViewController.swift
@@ -8,11 +8,41 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
-
+    
+    @IBOutlet weak var animalLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    let answers: [Answer]
+    
+    init?(coder: NSCoder, _ answers: [Answer]) {
+        self.answers = answers
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func calculatePersonalityResult() {
+        let frequancyOfAnswers = answers.reduce(into: [:]) { counts, answer in
+            counts[answer.type, default: 0] += 1
+        }
+        let frequancyOfAnswersSorted = frequancyOfAnswers.sorted { pair1, pair2 in
+            pair1.value > pair2.value
+        }
+        let mostCommonAnswer = frequancyOfAnswersSorted.first!.key
+        updateUI(mostCommonAnswer)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        calculatePersonalityResult()
+        navigationItem.hidesBackButton = true
+    }
+    
+    func updateUI(_ animal: AnimalType) {
+        animalLabel.text = "Вы - это \(animal.rawValue)"
+        descriptionLabel.text = animal.definition
     }
     
 
